1573311c72489a71a249382dabc7f65c87c946cc
cono1 <danielayg616@gmail.com>
2022-09-19 22:48:03 -0300

    primer prueba menu

--- main.cpp
+++ main.cpp
@@ -3,40 +3,92 @@
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    bool showMenu = true;
+    int menuMouseHover = 0;
+    int menuOptionSelected = 0;
+    const int menuSize = 3;    
+    Rectangle menuRect[menuSize];
+    const int screenWidth = 1000;
+    const int screenHeight = 800;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "asteroids");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+    for (int i = 0; i < menuSize; i++)
+    { 
+        menuRect[i].width = 270;
+        menuRect[i].height = 70;
+        menuRect[i].x = 350;
+        menuRect[i].y = 200 + menuRect[i].height * i + 50 * i;
+       
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    while (!WindowShouldClose())   
+    {
+        Vector2 mousePos = GetMousePosition();
 
+        for (int i = 0; i < menuSize; i++)
+        {
+            if (CheckCollisionPointRec(mousePos, menuRect[i]))
+            {
+                menuMouseHover = i;
+                break;
+            }
+            else
+                menuMouseHover = -1;
+        }
+
+        if (menuMouseHover >= 0 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            menuOptionSelected = menuMouseHover;
+        }
+
+    BeginDrawing();
+        ClearBackground(BLACK);
+        
+        if (showMenu)
+        {
+            for (int i = 0; i < menuSize; i++)
+            {
+                DrawRectangle(menuRect[i].x, menuRect[i].y, menuRect[i].width, menuRect[i].height, RED);
+                if (i == 0)
+                {
+                     DrawText("PLAY", menuRect[i].x + 35, menuRect[i].y + 10, 50, LIGHTGRAY);
+                     
+                }
+                   
+                if(i == 1)
+                    DrawText("CREDITS", menuRect[i].x, menuRect[i].y + 10, 50, LIGHTGRAY);
+                if (i == 2)
+                    DrawText("EXIT", menuRect[i].x + 35, menuRect[i].y + 10, 50, LIGHTGRAY);
+            }
+        }
+        else
+        {
+            switch (menuOptionSelected)
+            {
+            case 0:
+                DrawText("JUGANDO", 10, 10, 50, LIGHTGRAY);
+                showMenu = false;
+                break;
+            case 1:
+                DrawText("CREDITOS", 10, 10, 50, LIGHTGRAY);
+                showMenu = false;
+                break;
+            case 2:
+                DrawText("SALINEDO", 10, 10, 50, LIGHTGRAY);
+                showMenu = false;
+                break;
+            default:
+                DrawText("COSO", 10, 10, 50, LIGHTGRAY);
+                showMenu = false;
+                break;
+            }
+        }
+        
+
+        EndDrawing();
+    }
+    CloseWindow();       
     return 0;
 }
