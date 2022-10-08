52306e4393e8ffa4cd03494ce8e7926e0bd2f6b7
cono1 <danielayg616@gmail.com>
2022-10-08 10:55:12 -0300

    bullet movement improved

--- bullets.cpp
+++ bullets.cpp
@@ -2,15 +2,15 @@
 #include "bullets.h"
 #include "ship.h"
 
-Bullet bullet; //(? cuando funke hacer array (?
+Bullet bullet; 
 extern SpaceShip ship;
 
 void initBullet()
 {
     bullet.pos.x = ship.pos.x;
     bullet.pos.y = ship.pos.y;
-    bullet.newPos.x = 0;
-    bullet.newPos.y = 0;
+    //bullet.newPos.x = 0;
+    //bullet.newPos.y = 0;
     bullet.dir.x = ship.dir.x;
     bullet.dir.y = ship.dir.y;
     bullet.size.x = 10;
@@ -19,26 +19,32 @@ void initBullet()
 
 void shoot(Vector2& pos)
 {
-    bullet.newPos.x = pos.x + GetMouseX() * GetFrameTime(); //bullet.pos (??
-    bullet.newPos.y = pos.y + GetMouseY() * GetFrameTime();
-
-    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !bullet.isAlive)
     {
-        //std::cout << "x " << pos.x ;
-        //std::cout << " y " <<pos.y << std::endl;
-
-        pos.x = bullet.newPos.x;
-        pos.y = bullet.newPos.y;
-        if (pos.x >= GetScreenWidth() / 2)
-        {
-            std::cout << "\n\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n\n";
-        }
-        DrawRectangleV(pos, bullet.size, WHITE);
+        pos.x = bullet.pos.x;
+        pos.y = bullet.pos.y;
+        bullet.isAlive = true;
+        bullet.dir.x = GetMouseX() - ship.pos.x;
+        bullet.dir.y = GetMouseY() - ship.pos.y;
+        bullet.dir = Vector2Normalize(bullet.dir);
+        bullet.speed = 500;
     }
+
+    if (!bullet.isAlive)
+    {
+        return;
+    }
+
+    bullet.pos.x += bullet.dir.x * bullet.speed * GetFrameTime(); 
+    bullet.pos.y += bullet.dir.y * bullet.speed * GetFrameTime();
+
+    
+    DrawRectangleV(pos, bullet.size, WHITE);
 
     if (pos.x < 0 || pos.x > GetScreenWidth() || pos.y < 0 || pos.x > GetScreenWidth())
     {
         pos.x = ship.pos.x;
         pos.y = ship.pos.y;
+        bullet.isAlive = false;
     }
 }
--- bullets.h
+++ bullets.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <raylib.h>
+#include <raymath.h>
 
 struct Bullet
 {
 	Vector2 pos;
-	Vector2 newPos;
+	//Vector2 newPos;
 	Vector2 dir;
 	Vector2 size;
+	float speed;
+	bool isAlive;
 };
 
 void initBullet();
